4d468b5920712f74c5220a88cce7bcb3f07a6b16
August (Chris) <augu@voided.pw>
2019-11-19 16:13:27 -0700

    chore(0.0.4): update changes

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
 declare module '@augu/sysinfo' {
+    type FreeFlag = 'bytes' | 'kilobytes' | 'megabytes' | 'gigabytes' | 'terabytes' | 'petabytes';
     interface FreeInfo {
         total: number;
         free: number;
@@ -7,12 +8,19 @@ declare module '@augu/sysinfo' {
         used: number;
     }
 
+    interface Constants {
+        FreeFlags: { 
+            [x in FreeFlag]: string;
+        }
+    }
+
     export const version: string;
+    export const Constants: Constants;
     export function getPlatform(): 'Linux' | 'Macintosh' | 'Windows' | 'Android' | 'Unknown';
     export function getCpuCount(): number;
     export function getFreeMemory(): number;
     export function getTotalMemory(): number;
-    export function free(): FreeInfo;
+    export function free(type: FreeFlag): FreeInfo;
     export function getLoadAvg(time?: number): number;
     export function getCpuUsage(): number;
     export function getCpuInfo(): { total: number; idle: number; }
