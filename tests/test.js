9f660ef4be4308951e5fd3f9252bd450772c113f
Noel <augu@voided.pw>
2020-03-31 07:00:00 -0700

    oops

--- tests/test.js
+++ tests/test.js
@@ -13,14 +13,14 @@ async function testerino() {
   console.log(`System Total Memory: ${sys.getTotalMemory()}`);
   console.log(`System CPU Usage: ${sys.getCPUUsage()}%`);
   // it's intentional to emit a warning
-  //console.log(`System Memory by Megabytes:\n${inspect(sys.free(sys.Constants.FreeFlags.megabytes))}`);
-  //console.log(`System Memory by Gigabytes:\n${inspect(sys.free('gigabytes'))}\n`);
+  console.log(`System Memory by Megabytes:\n${inspect(sys.free(sys.Constants.FreeFlags.megabytes))}`);
+  console.log(`System Memory by Gigabytes:\n${inspect(sys.free('gigabytes'))}\n`);
   console.log(`System Load Average by 1: ${sys.getLoadAvg(1)}`);
   console.log(`System Load Average by 5: ${sys.getLoadAvg(5)}`);
   console.log(`System CPU Model (${sys.getCPUInfo().firstModel}...${sys.getCPUInfo().lastModel}):\n${inspect(sys.getCPUInfo())}\n`);
-  //console.log(`System Filesystem:\n${inspect(sys.getFilesystemInfo())}\n`);
+  console.log(`System Filesystem:\n${inspect(sys.getFilesystemInfo())}\n`);
   console.log(`System Proccess by 10:\n${inspect(await sys.getProcesses(10))}\n`);
-  //console.log(`Unix Uptime:\n${inspect(sys.getUnixUptime())}`);
+  console.log(`Unix Uptime:\n${inspect(sys.getUnixUptime())}`);
   console.log(`Workstation: ${inspect(sys.getWindowsWorkstation())}`);
   console.log(`Services: ${inspect(sys.getWindowsServices())}`);
   console.log(`Motherboard: ${inspect(sys.getMotherboard())}`);
